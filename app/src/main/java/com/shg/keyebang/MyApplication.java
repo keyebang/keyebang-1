1d1204c3e9edcd1bfbcbfdfeb9bde958643c4fab
DESKTOP-5H96181\Kaltome <Kaltomesss@gmail.com>
2019-11-16 00:51:45 +0800

    Empty Project

---
+++ app/src/main/java/com/shg/keyebang/MyApplication.java
@@ -0,0 +1,13 @@
+package com.shg.keyebang;
+
+import android.app.Application;
+
+import cn.bmob.v3.Bmob;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Bmob.initialize(this, BuildConfig.APPKEY);
+    }
+}
