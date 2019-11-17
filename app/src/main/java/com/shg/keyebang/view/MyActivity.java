cb6c6ef08965f81df16be40e244225d3e4e1021b
DESKTOP-5H96181\Kaltome <Kaltomesss@gmail.com>
2019-11-17 17:16:10 +0800

    Add DensityAdapter to Set DisplayWidth to 360dp in Every Device

--- app/src/main/java/com/shg/keyebang/view/MyActivity.java
+++ app/src/main/java/com/shg/keyebang/view/MyActivity.java
@@ -1,7 +1,15 @@
 package com.shg.keyebang.view;
 
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.widget.Toast;
+
+import com.shg.keyebang.builder.DensityAdapter;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public abstract class MyActivity extends AppCompatActivity {
@@ -9,8 +17,16 @@ public abstract class MyActivity extends AppCompatActivity {
 
     abstract protected void init();
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        DensityAdapter.setCustomDensity(this, getApplication());
+    }
+
     public void toastAndLog(String message){
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
         Log.d(TAG, message);
     }
+
+
 }
