3c45bf4561e6bdac10e47daf8f95cf79f93ec9af
yansikuang <39219559+yansikuang@users.noreply.github.com>
2019-11-28 19:46:22 +0800

    no message

--- app/src/main/java/com/shg/keyebang/services/account/PhoneListener.java
+++ app/src/main/java/com/shg/keyebang/services/account/PhoneListener.java
@@ -1,8 +1,7 @@
 package com.shg.keyebang.services.account;
 
 public abstract class PhoneListener {
-    public abstract void loginSuccess(String message);
-    public abstract void signinSuccess();
+
     public abstract void onFailure(String errMessage);
     public abstract void phoneSuccess(String Message);
 
