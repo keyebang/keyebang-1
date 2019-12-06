c76deb856cbf94f780263e5b72823cad1e5fadfe
yansikuang <39219559+yansikuang@users.noreply.github.com>
2019-12-07 00:07:32 +0800

    no message

--- app/src/main/java/com/shg/keyebang/services/account/SignUpLogInListener.java
+++ app/src/main/java/com/shg/keyebang/services/account/SignUpLogInListener.java
@@ -2,7 +2,7 @@ package com.shg.keyebang.services.account;
 
 import com.shg.keyebang.model.User;
 public abstract class SignUpLogInListener {
-    public abstract void onSuccess(User user,String message);
+    public abstract void onSuccess(String message);
 
     public abstract void onFailure(String errMessage);
 }
