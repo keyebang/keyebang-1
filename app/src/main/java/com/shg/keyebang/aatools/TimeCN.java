a99d2f932c04bd007a2575bf7368de7738d4aced
DESKTOP-5H96181\Kaltome <Kaltomesss@gmail.com>
2019-12-08 02:35:53 +0800

    Fix Bug About Weekday

--- app/src/main/java/com/shg/keyebang/aatools/TimeCN.java
+++ app/src/main/java/com/shg/keyebang/aatools/TimeCN.java
@@ -10,13 +10,13 @@ public class TimeCN {
 
     public static String weekdayToCN(int i){
         switch (i){
-            case 1: return "一";
-            case 2: return "二";
-            case 3: return "三";
-            case 4: return "四";
-            case 5: return "五";
-            case 6: return "六";
-            case 7: return "日";
+            case 1: return "日";
+            case 2: return "一";
+            case 3: return "二";
+            case 4: return "三";
+            case 5: return "四";
+            case 6: return "五";
+            case 7: return "六";
         }
         return i + "";
     }
